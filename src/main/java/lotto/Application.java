d6ceb2ee7b31a5329f47fa730dee0f7021f75948
신동훈 <huipulci1@naver.com>
2024-11-02 17:44:56 +0900

    refactor: main 문 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -15,7 +15,12 @@ public class Application {
     public static void main(String[] args) {
         NumberFormat numberFormat = NumberFormat.getInstance(Locale.KOREA);
         LottoSeller lottoSeller = new LottoSeller();
+        List<Lotto> lottos = purchaseLotto(lottoSeller);
+        WinningNumbers winningNumbers = inputWinningNumbers();
+        printWinningStatistics(lottos, winningNumbers, numberFormat);
+    }
 
+    private static List<Lotto> purchaseLotto(LottoSeller lottoSeller) {
         System.out.println("구입금액을 입력해 주세요");
         int amount = Integer.parseInt(Console.readLine());
         List<Lotto> lottos = lottoSeller.sell(amount);
@@ -23,37 +28,42 @@ public class Application {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+        return lottos;
+    }
 
+    private static WinningNumbers inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         Set<Integer> winningNumberInputs = Arrays.stream(Console.readLine().split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toSet());
         WinningNumbers winningNumbers = new WinningNumbers(winningNumberInputs);
         System.out.println("보너스 번호를 입력해 주세요.");
-        LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(Console.readLine()));;
+        LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(Console.readLine()));
+        ;
         winningNumbers.withBonusNumber(bonusNumber);
+        return winningNumbers;
+    }
 
+    private static void printWinningStatistics(List<Lotto> lottos, WinningNumbers winningNumbers,
+                                               NumberFormat numberFormat) {
         Map<Winning, Integer> winnings = new HashMap<>();
         for (Lotto lotto : lottos) {
             Winning winning = winningNumbers.match(lotto);
             Integer count = winnings.getOrDefault(winning, 0);
             winnings.put(winning, count + 1);
         }
-
         System.out.println("당첨 통계");
         System.out.println("---");
         for (Winning winning : Winning.values()) {
             Integer count = winnings.getOrDefault(winning, 0);
+            System.out.println(winning + "- %d개".formatted(count));
             if (winning == Winning.SECOND) {
                 System.out.println("%d개 일치, 보너스 볼 일치 (%s)원 - %d개".formatted(
                         winning.getMatchCount(),
                         numberFormat.format(winning.getWinningAmount()),
                         count
                 ));
-            }
-            if (winning == Winning.NONE) {
-                continue;
-            } else {
+            } else if (winning != Winning.NONE) {
                 System.out.println("%d개 일치 (%s)원 - %d개".formatted(
                         winning.getMatchCount(),
                         numberFormat.format(winning.getWinningAmount()),
@@ -61,6 +71,7 @@ public class Application {
                 ));
             }
         }
+        // 아 귀찮노..
         System.out.println("총 수익률은 %d\\%입니다.");
     }
 }
