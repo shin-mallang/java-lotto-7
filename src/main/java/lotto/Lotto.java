0d1e04e39d902e7a18f2ec7948b027c140dc0710
신동훈 <huipulci1@naver.com>
2024-11-02 16:33:24 +0900

    feat: 로또 번호가 중복되지 않도록 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -30,5 +30,9 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.");
             }
         }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numbers.size() != numberSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되어서는 안됩니다.");
+        }
     }
 }
